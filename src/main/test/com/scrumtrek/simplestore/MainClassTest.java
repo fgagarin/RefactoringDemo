852bc9a54b1d30fe4d22c1ed849f998f91025543
Fedor Gagarin <gagarin@angstrem.ru>
2015-10-18 18:13:17 +0300

    add primitive test for legacy code

---
+++ src/main/test/com/scrumtrek/simplestore/MainClassTest.java
@@ -0,0 +1,49 @@
+package com.scrumtrek.simplestore;
+
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNull;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.*;
+
+/**
+ * Created by fgagarin on 10/18/15.
+ */
+public class MainClassTest {
+
+    @Test
+    public void testMain() throws Exception {
+        // Create movies
+        Movie movCinderella = new Movie("Cinderella", PriceCodes.Childrens);
+        Movie movStarWars = new Movie("Star Wars", PriceCodes.Regular);
+        Movie movGladiator = new Movie("Gladiator", PriceCodes.NewRelease);
+
+        // Create customers
+        Customer custMickeyMouse = new Customer("Mickey Mouse");
+        Customer custDonaldDuck = new Customer("Donald Duck");
+        Customer custMinnieMouse = new Customer("Minnie Mouse");
+
+        // Create rentals
+        Rental rental1 = new Rental(movCinderella, 5);
+        Rental rental2 = new Rental(movStarWars, 5);
+        Rental rental3 = new Rental(movGladiator, 5);
+
+        // Assign rentals to customers
+        custMickeyMouse.addRental(rental1);
+        custMickeyMouse.addRental(rental2);
+        custMickeyMouse.addRental(rental3);
+
+        // Generate invoice
+        String statement = custMickeyMouse.Statement();
+
+        // Print the statement
+        assertThat(statement, notNullValue());
+        assertThat(statement, IsEqual.equalTo("Rental record for Mickey Mouse\n" +
+                "\tCinderella\t3.0\n" +
+                "\tStar Wars\t6.5\n" +
+                "\tGladiator\t15.0\n" +
+                "Amount owed is 24.5\n" +
+                "You earned 4 frequent renter points."));
+    }
+}
